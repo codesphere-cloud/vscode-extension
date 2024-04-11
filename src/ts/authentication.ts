0ae2a3b315f4121d89c6aebec744e0e38b1c5533
Datata1 <wija1025@h-ka.de>
2024-04-11 23:48:28 +0200

    switched from request to axios

--- src/ts/authentication.ts
+++ src/ts/authentication.ts
@@ -1,4 +1,4 @@
-import * as request from "request";
+import axios from "axios";
 import * as vscode from "vscode";
 
 // Funktion zum Anmelden
@@ -12,67 +12,56 @@ export function signIn(email: string, password: string, callback: (error: any, s
   
     console.log('Signing in...');
   
-    request.post({
-        url: signInUrl,
-        json: requestData
-    }, (error, response, body) => {
-        if (error) {
-          console.log('error1', error);
-          
-            callback(new Error(body.errMessage)); // Fehler an die Callback-Funktion übergeben
-            return;
-        }
-  
-        if (response.statusCode === 200) {
-          console.log('body', body);
-            if (body.code === "Error") {
-                callback(new Error(body.errMessage)); // Fehler an die Callback-Funktion übergeben
-                return;
+    axios.post(signInUrl, requestData)
+        .then(response => {
+            const { data, status } = response;
+            if (status === 200) {
+                console.log('body', data);
+                if (data.code === "Error") {
+                    callback(new Error(data.errMessage)); // Fehler an die Callback-Funktion übergeben
+                    return;
+                }
+                const sessionId = data.data.sessionId;
+                callback(null, sessionId); // sessionId an die Callback-Funktion übergeben
+            } else {
+                console.log('error3', response);
+                callback(new Error(response.statusText)); // Fehler an die Callback-Funktion übergeben
             }
-            const sessionId = body.data.sessionId;
-            callback(null, sessionId); // sessionId an die Callback-Funktion übergeben
-        } else {
-          console.log('error3', response);
-            callback(new Error(response.statusMessage)); // Fehler an die Callback-Funktion übergeben
-        }
-    });
+        })
+        .catch(error => {
+            console.log('error1', error);
+            callback(error); // Fehler an die Callback-Funktion übergeben
+        });
   
     console.log('signed in');
-  }
-  
-  
+}
+
 // Funktion zum Generieren des Access Tokens
 export function genAccessToken(sessionId: string, callback: (error: any, accessToken?: string) => void) {
-const genAccessTokenUrl = 'https://codesphere.com/auth-service/genAccessToken';
+    const genAccessTokenUrl = 'https://codesphere.com/auth-service/genAccessToken';
 
-const requestData = {
-    id: sessionId
-};
+    const requestData = {
+        id: sessionId
+    };
 
-console.log('Generating access token...');
+    console.log('Generating access token...');
 
-request.post({
-    url: genAccessTokenUrl,
-    json: requestData
-}, (error, response, body) => {
-    if (error) {
-        console.log('error', error);
-        console.log('body', body);
-        callback(error);
-        vscode.window.showErrorMessage('An error occurred while generating access token: ' + error.message);
-        return;
-    }
-
-    if (response.statusCode === 200) {
-        vscode.window.showInformationMessage(`Successfully signed in2`);
-        console.log('body', body);
-        const accessToken = body.data.accessToken;
-        callback(null, accessToken); // sessionId an die Callback-Funktion übergeben
-
-
-    } else {
-        vscode.window.showErrorMessage('Failed to generate access token: ' + body.errMessage);
-        callback(new Error(body.errMessage));
-    }
-});
+    axios.post(genAccessTokenUrl, requestData)
+        .then(response => {
+            const { data, status } = response;
+            if (status === 200) {
+                vscode.window.showInformationMessage(`Successfully signed in2`);
+                console.log('body', data);
+                const accessToken = data.data.accessToken;
+                callback(null, accessToken); // accessToken an die Callback-Funktion übergeben
+            } else {
+                vscode.window.showErrorMessage('Failed to generate access token: ' + data.errMessage);
+                callback(new Error(data.errMessage));
+            }
+        })
+        .catch(error => {
+            console.log('error', error);
+            callback(error); // Fehler an die Callback-Funktion übergeben
+            vscode.window.showErrorMessage('An error occurred while generating access token: ' + error.message);
+        });
 }
