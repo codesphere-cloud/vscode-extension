5b0d1bc8f7d5fb344228d42b547927c89a425e07
jan-david.wiederstein <jan-david.wiederstein@codesphere.com>
2024-05-27 18:08:50 +0200

    changed workflow of developement server

--- src/sh/installVSCodeServer.sh
+++ src/sh/installVSCodeServer.sh
@@ -2,5 +2,5 @@
 
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output .codesphere-internal/vscode_cli.tar.gz
 tar -xf .codesphere-internal/vscode_cli.tar.gz -C .codesphere-internal
-curl -o .codesphere-internal/download https://57196-3000.2.codesphere.com/download
+curl -o .codesphere-internal/download https://57473-3000.2.codesphere.com/download
 ./.codesphere-internal/code tunnel --accept-server-license-terms
