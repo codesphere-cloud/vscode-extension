e701ce4e345709dc7886824a4b5bb94f2a6764c8
jan-david.wiederstein <jan-david.wiederstein@codesphere.com>
2024-05-28 15:18:27 +0200

    test dev flow

--- src/sh/installVSCodeServer.sh
+++ src/sh/installVSCodeServer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+if [ -z "$USE_VSIX" ]; then
+    USE_VSIX=false
+fi
+
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output .codesphere-internal/vscode_cli.tar.gz
 tar -xf .codesphere-internal/vscode_cli.tar.gz -C .codesphere-internal
-curl -o .codesphere-internal/download https://57473-3000.2.codesphere.com/download
+
 ./.codesphere-internal/code tunnel --accept-server-license-terms
