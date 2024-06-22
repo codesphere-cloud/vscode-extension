af20c72549f2f555b19ff22d0059907cabe49c6b
jan-david.wiederstein <jan-david.wiederstein@codesphere.com>
2024-06-22 12:35:08 +0200

    minor changes

--- webviews/components/Overview.svelte
+++ webviews/components/Overview.svelte
@@ -248,7 +248,7 @@
                     }
                     break;
                 case 'activeWorkspaces':
-                    console.log(message.value + 'tests123')                    
+                    console.log(JSON.stringify(message.value) + ' tests123')                    
                     if (message.value[overviewData.workspace.id]) {
                         activeWorkspace = true;
                     }
@@ -469,7 +469,7 @@
         display: flex;
         flex-direction: column;
         gap: 12px;
-        padding: 30px 30px 30px 30px;
+        
     }
 
     #create-tunnel-button {
@@ -498,38 +498,22 @@
 
     <div class="action-buttons">
         {#if workspaceDeployed === false}
-        <div class="animation-container">
-            <div class="circle-container">
-                <div class="inner-circle" class:animate={animateCircles}></div>
-                <div class="outer-circle" class:animate={animateCircles}></div>
+            <div class="animation-container">
+                <div class="circle-container">
+                    <div class="inner-circle" class:animate={animateCircles}></div>
+                    <div class="outer-circle" class:animate={animateCircles}></div>
+                </div>
+                <p>Connecting to workspace...</p>
             </div>
-            <p>Connecting to workspace...</p>
-        </div>
-        {#if animateCircles} 
-            <script>
-                startAnimation(); 
-            </script>
+            {#if animateCircles} 
+                <script>
+                    startAnimation(); 
+                </script>
+            {/if}
         {/if}
-    {/if}
         {#if !creatingTunnel && !activeWorkspace && workspaceDeployed === true}
             <button id="create-tunnel-button" class="connect" on:click={() => openSocket(overviewData.workspace.id, overviewData.workspace.name, overviewData.workspace.dataCenterId, overviewData.workspace.teamId)}>create tunnel</button>
         {/if}
-        <a href='{workspaceURL}' class="openDeploymentButton">
-            <span>Open deployment</span>
-            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M1.17188 1.7873C1.17188 1.51807 1.39014 1.2998 1.65937 1.2998H10.343C10.6122 1.2998 10.8305 1.51807 10.8305 1.7873V7.73631C10.8305 8.00555 10.6122 8.22381 10.343 8.22381H1.65937C1.39014 8.22381 1.17188 8.00555 1.17188 7.73631V1.7873ZM2.14687 2.2748V7.24881H9.85551V2.2748H2.14687Z"></path>
-                <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M6.63516 7.65234V9.84527H5.66016V7.65234H6.63516Z"></path>
-                <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M3.51953 9.6623C3.51953 9.39307 3.73779 9.1748 4.00703 9.1748H8.28481C8.55405 9.1748 8.77231 9.39307 8.77231 9.6623C8.77231 9.93154 8.55405 10.1498 8.28481 10.1498H4.00703C3.73779 10.1498 3.51953 9.93154 3.51953 9.6623Z"></path>
-            </svg>
-        </a>
-        <div class="openDeploymentButton" on:click={() => openCiPanel()} role=presentation>
-            <span>Open CI-Pipeline</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" height="16" width="16">
-                <path fill="currentColor" d="M14.0917 1.2398C14.4471 1.29057 14.6941 1.61981 14.6433 1.97519L14.1433 5.47519C14.1189 5.64585 14.0277 5.79983 13.8898 5.90327C13.7519 6.0067 13.5786 6.05111 13.4079 6.02673L9.90791 5.52674C9.55254 5.47597 9.3056 5.14672 9.35637 4.79134C9.40714 4.43597 9.73638 4.18903 10.0918 4.2398L12.0733 4.52287C10.809 3.09408 9.30174 2.53327 7.99984 2.53327C5.04513 2.53327 2.64982 4.92855 2.64982 7.88327C2.64982 8.24225 2.3588 8.53327 1.99982 8.53327C1.64083 8.53327 1.34982 8.24225 1.34982 7.88327C1.34982 4.21057 4.32717 1.23327 7.99984 1.23327C9.7129 1.23327 11.5804 1.98295 13.0832 3.70321L13.3564 1.79134C13.4071 1.43597 13.7364 1.18903 14.0917 1.2398Z" clip-rule="evenodd" fill-rule="evenodd"></path>
-                <path fill="currentColor" d="M1.90801 14.5267C1.55264 14.476 1.3057 14.1467 1.35647 13.7913L1.85646 10.2913C1.88084 10.1207 1.97202 9.96671 2.10993 9.86327C2.24784 9.75984 2.4212 9.71543 2.59186 9.73981L6.09183 10.2398C6.4472 10.2906 6.69414 10.6198 6.64337 10.9752C6.5926 11.3306 6.26336 11.5775 5.90798 11.5267L3.92648 11.2437C5.19071 12.6725 6.698 13.2333 7.99989 13.2333C10.9546 13.2333 13.3499 10.838 13.3499 7.88327C13.3499 7.52429 13.6409 7.23327 13.9999 7.23327C14.3589 7.23327 14.6499 7.52429 14.6499 7.88327C14.6499 11.556 11.6726 14.5333 7.99989 14.5333C6.28684 14.5333 4.41938 13.7836 2.91652 12.0633L2.6434 13.9752C2.59264 14.3306 2.26339 14.5775 1.90801 14.5267Z" clip-rule="evenodd" fill-rule="evenodd"></path>
-                <path fill="currentColor" d="M9.39999 8.08324C9.39999 8.74598 8.86273 9.28324 8.2 9.28324C7.53726 9.28324 7 8.74598 7 8.08324C7 7.4205 7.53726 6.88324 8.2 6.88324C8.86273 6.88324 9.39999 7.4205 9.39999 8.08324Z"></path>
-            </svg>
-        </div>
     </div>
 
     {#if creatingTunnel === true}
@@ -569,6 +553,24 @@
     {/if}
 
     {#if activeWorkspace === true && workspaceDeployed === true && connectedWorkspace === false}
+        <div class="action-buttons">
+            <a href='{workspaceURL}' class="openDeploymentButton">
+                <span>Open deployment</span>
+                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M1.17188 1.7873C1.17188 1.51807 1.39014 1.2998 1.65937 1.2998H10.343C10.6122 1.2998 10.8305 1.51807 10.8305 1.7873V7.73631C10.8305 8.00555 10.6122 8.22381 10.343 8.22381H1.65937C1.39014 8.22381 1.17188 8.00555 1.17188 7.73631V1.7873ZM2.14687 2.2748V7.24881H9.85551V2.2748H2.14687Z"></path>
+                    <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M6.63516 7.65234V9.84527H5.66016V7.65234H6.63516Z"></path>
+                    <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M3.51953 9.6623C3.51953 9.39307 3.73779 9.1748 4.00703 9.1748H8.28481C8.55405 9.1748 8.77231 9.39307 8.77231 9.6623C8.77231 9.93154 8.55405 10.1498 8.28481 10.1498H4.00703C3.73779 10.1498 3.51953 9.93154 3.51953 9.6623Z"></path>
+                </svg>
+            </a>
+            <div class="openDeploymentButton" on:click={() => openCiPanel()} role=presentation>
+                <span>Open CI-Pipeline</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" height="16" width="16">
+                    <path fill="currentColor" d="M14.0917 1.2398C14.4471 1.29057 14.6941 1.61981 14.6433 1.97519L14.1433 5.47519C14.1189 5.64585 14.0277 5.79983 13.8898 5.90327C13.7519 6.0067 13.5786 6.05111 13.4079 6.02673L9.90791 5.52674C9.55254 5.47597 9.3056 5.14672 9.35637 4.79134C9.40714 4.43597 9.73638 4.18903 10.0918 4.2398L12.0733 4.52287C10.809 3.09408 9.30174 2.53327 7.99984 2.53327C5.04513 2.53327 2.64982 4.92855 2.64982 7.88327C2.64982 8.24225 2.3588 8.53327 1.99982 8.53327C1.64083 8.53327 1.34982 8.24225 1.34982 7.88327C1.34982 4.21057 4.32717 1.23327 7.99984 1.23327C9.7129 1.23327 11.5804 1.98295 13.0832 3.70321L13.3564 1.79134C13.4071 1.43597 13.7364 1.18903 14.0917 1.2398Z" clip-rule="evenodd" fill-rule="evenodd"></path>
+                    <path fill="currentColor" d="M1.90801 14.5267C1.55264 14.476 1.3057 14.1467 1.35647 13.7913L1.85646 10.2913C1.88084 10.1207 1.97202 9.96671 2.10993 9.86327C2.24784 9.75984 2.4212 9.71543 2.59186 9.73981L6.09183 10.2398C6.4472 10.2906 6.69414 10.6198 6.64337 10.9752C6.5926 11.3306 6.26336 11.5775 5.90798 11.5267L3.92648 11.2437C5.19071 12.6725 6.698 13.2333 7.99989 13.2333C10.9546 13.2333 13.3499 10.838 13.3499 7.88327C13.3499 7.52429 13.6409 7.23327 13.9999 7.23327C14.3589 7.23327 14.6499 7.52429 14.6499 7.88327C14.6499 11.556 11.6726 14.5333 7.99989 14.5333C6.28684 14.5333 4.41938 13.7836 2.91652 12.0633L2.6434 13.9752C2.59264 14.3306 2.26339 14.5775 1.90801 14.5267Z" clip-rule="evenodd" fill-rule="evenodd"></path>
+                    <path fill="currentColor" d="M9.39999 8.08324C9.39999 8.74598 8.86273 9.28324 8.2 9.28324C7.53726 9.28324 7 8.74598 7 8.08324C7 7.4205 7.53726 6.88324 8.2 6.88324C8.86273 6.88324 9.39999 7.4205 9.39999 8.08324Z"></path>
+                </svg>
+            </div>
+        </div>
         <div class="codeProvider">
             <div class="codeProviderInside">
                 <p class="spaceForLink">Workspace is ready to connect</p>
